7290234a364de7b6a48190d2cfa6b40d97f9d93c
Max Goodwin <maxgoodwin3@pm.me>
2026-08-19 17:35:38 +1000

    Bump to v0.1.2 (menu header thumbnail fix)

--- Formula/spotlight-wallpaper.rb
+++ Formula/spotlight-wallpaper.rb
@@ -1,8 +1,8 @@
 class SpotlightWallpaper < Formula
   desc "Menu bar app: real Windows Spotlight daily wallpapers on macOS, with photo info"
   homepage "https://github.com/maxgoodwin/spotlight-wallpaper"
-  url "https://github.com/maxgoodwin/spotlight-wallpaper/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "80baa72a16b7384bb9dc2eae0da5066f702fdedeb331d17af8526dc28ce6cf20"
+  url "https://github.com/maxgoodwin/spotlight-wallpaper/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "7fea304401d79242c542234949c5069a6395bcb52e70afc5124a9d15f9ab55d6"
   license "GPL-3.0-or-later"
 
   depends_on macos: :sonoma
